612c0af070923e5bea3686e529b6039efac8a7a2
matthew <matthew@foursquare.com>
2013-01-26 12:08:41 -0600

    first job launcher

--- java-mapreduce/src/main/java/com/matthewrathbone/example/RawMapreduce.java
+++ java-mapreduce/src/main/java/com/matthewrathbone/example/RawMapreduce.java
@@ -7,21 +7,44 @@ import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.mapreduce.lib.input.MultipleInputs;
 import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
 import java.io.IOException;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.mapreduce.lib.output.SequenceFileOutputFormat;
 
 public class RawMapreduce 
 {
-    public static void main( String[] args ) throws IOException {
+    public static void main( String[] args ) throws Exception {
         Path inputTransactions = new Path(args[0]);
         Path inputUsers = new Path(args[1]);
-        Path output = new Path(args[2]);
+        Path staging = new Path(args[2]);
+        Path output = new Path(args[3]);
 
+        runFirstJob(inputTransactions, inputUsers, staging);
+        runSecondJob(staging, output);
+
+        
+    }
+
+    private static void runFirstJob(Path transactions, Path users, Path output) 
+    throws Exception {
         Job job = new Job(new Configuration());
         job.setJarByClass(RawMapreduce.class);
         job.setJobName("raw Mapreduce Example");
-        MultipleInputs.addInputPath(job, inputTransactions, TextInputFormat.class, TransactionMapper.class);
-        MultipleInputs.addInputPath(job, inputUsers, TextInputFormat.class, UserMapper.class);
+        MultipleInputs.addInputPath(job, transactions, TextInputFormat.class, TransactionMapper.class);
+        MultipleInputs.addInputPath(job, users, TextInputFormat.class, UserMapper.class);
         job.setPartitionerClass(SecondarySort.SSPartitioner.class);
         job.setGroupingComparatorClass(SecondarySort.SSGroupComparator.class);
         job.setSortComparatorClass(SecondarySort.SSSortComparator.class);
+        job.setReducerClass(JoinReducer.class);
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(Text.class);
+        job.setOutputFormatClass(SequenceFileOutputFormat.class);
+        FileOutputFormat.setOutputPath(job, output);
+        if (job.waitForCompletion(true)) return;
+        else throw new Exception("First Job Failed");
+    }
+
+    private static void runSecondJob(Path input, Path output) {
+
     }
 }
